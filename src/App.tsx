a0e921aa165493ff5aa2fed62785d03f36c50cf3
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-03-11 08:03:57 +0800

    Minor Changes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,36 @@ import "./App.css";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Header from "./components/Header/Header";
 import Items from "./pages/Items/Items";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <div className="main w-full h-full">
-                    <section className="w-full h-full flex">
-                        <NavigationBar />
-                        <section className="content flex flex-col flex-1 h-full">
-                            <Header />
+  
 
-                            {/* Main - Routes */}
-                            <div className="flex-1 mt-[80px] md:ml-[270px]">
-                              <Routes>
-                                <Route path="/dashboard" element={<Dashboard />} />
-                                  <Route path="/items" element={<Items />} />
-                              </Routes>
-                            </div>
-                        </section>
-                    </section>
-                </div>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <Router>
+      <div className="main w-full h-full">
+        <section className="w-full h-full flex">
+          <NavigationBar />
+          <section className="content flex flex-col flex-1 h-full">
+            <Header />
+
+            {/* Main - Routes */}
+            <div className="flex-1 mt-[80px] md:ml-[270px]">
+              <Routes>
+                <Route index element={<Dashboard />} />
+                <Route path="/items" element={<Items />} />
+              </Routes>
+            </div>
+          </section>
+        </section>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ const Header = () => {
   const location = useLocation();
 
   const pageTitles: IPageType = {
+    '/': 'Dashboard',
     '/dashboard': 'Dashboard',
     '/items': 'Items'
   }
