e512a68f55dd9b006c7e771a4c49fbd3eb79a4d1
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-03-13 14:23:27 +0800

    Fixed items.

--- src/pages/Items/Items.tsx
+++ src/pages/Items/Items.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   LuLayoutGrid,
   LuLayoutList,
