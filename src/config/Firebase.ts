689fe6a0c70296d6c4599c19c4a6b10157808d40
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-03-18 12:56:01 +0800

    Update Firebase.ts

--- src/config/Firebase.ts
+++ src/config/Firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2eG_Y8kUPc68HxDoSPM4Tdn6UVMGrPms",
-  authDomain: "utak-menu-pos-3ebd7.firebaseapp.com",
-  databaseURL: "https://utak-menu-pos-3ebd7-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "utak-menu-pos-3ebd7",
-  storageBucket: "utak-menu-pos-3ebd7.appspot.com",
-  messagingSenderId: "163064075510",
-  appId: "1:163064075510:web:3d5fe12fba0e8ed5a0c8d7",
-  measurementId: "G-KKQ6Z5WRJ2"
+  apiKey: "AIzaSyDAOm6QOxIdEgSCIbn0T-lWIHCh-UZw0fo",
+  authDomain: "utak-item-menu.firebaseapp.com",
+  databaseURL: "https://utak-item-menu-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "utak-item-menu",
+  storageBucket: "utak-item-menu.appspot.com",
+  messagingSenderId: "730271258419",
+  appId: "1:730271258419:web:9db65b814e8a5236b11046",
+  measurementId: "G-VLQBGFHDEG"
 };
 
 // Initialize Firebase
