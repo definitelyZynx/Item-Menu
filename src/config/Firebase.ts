a572640cc24adef02ba356905eae8254f47568cc
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-03-19 13:51:53 +0800

    Update Firebase.ts

--- src/config/Firebase.ts
+++ src/config/Firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAggPuV5oK_pHtsy3d-0WsQ-X-4lUmeQmE",
