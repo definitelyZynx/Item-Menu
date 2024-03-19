8225d88771cf0f9467271b874cab390d3babf311
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-03-19 13:21:16 +0800

    Update Firebase.ts

--- src/config/Firebase.ts
+++ src/config/Firebase.ts
@@ -8,8 +8,8 @@ const firebaseConfig = {
   projectId: "utak-item-menu-2cb34",
   storageBucket: "utak-item-menu-2cb34.appspot.com",
   messagingSenderId: "118321879325",
-  appId: "1:118321879325:web:6a9a496b0d80e105d969f4",
-  measurementId: "G-QGFBJXJ2SE"
+  appId: "1:118321879325:web:5a4686d2401866cdd969f4",
+  measurementId: "G-JHTZ0MNBRB"
 };
 
 // Initialize Firebase
