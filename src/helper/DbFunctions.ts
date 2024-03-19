6bfe8d099ddf7a236b7af65ae853b96fe1e0f79f
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-03-19 17:00:11 +0800

    Added Auth to DbFunctions.ts

--- src/helper/DbFunctions.ts
+++ src/helper/DbFunctions.ts
@@ -2,46 +2,61 @@
 import { db } from "@/config/Firebase";
 import { ref, remove, set, update } from "firebase/database";
 import { v4 as uuidv4 } from 'uuid';
+import { auth } from "@/config/Firebase";
 
 // Insert Into the Database
 export const write_db = (refPath: string, data: any): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const uuid = uuidv4();
-    set(ref(db, `/${refPath}/${uuid}`), {
-      ...data,
-      uuid: uuid
-    }).then(() => {
-      resolve(uuid);
-    }).catch((error) => {
-      reject(error);
-    });
+    // Check if user is authenticated
+    if (auth.currentUser) {
+      const uuid = uuidv4();
+      set(ref(db, `/${refPath}/${uuid}`), {
+        ...data,
+        uuid: uuid
+      }).then(() => {
+        resolve(uuid);
+      }).catch((error) => {
+        reject(error);
+      });
+    } else {
+      reject(new Error("User not authenticated"));
+    }
   });
 };
 
 // Update the Database
 export const update_db = (refPath: string, itemId: string, data: any): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const itemRef = ref(db, `/${refPath}/${itemId}`);
-    update(itemRef, data)
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
+    // Check if user is authenticated
+    if (auth.currentUser) {
+      const itemRef = ref(db, `/${refPath}/${itemId}`);
+      update(itemRef, data)
+        .then(() => {
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    } else {
+      reject(new Error("User not authenticated")); 
+    }
   });
 };
-
 
 // Delete From the Database
 export const delete_db = (refPath: string, uuid: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    remove(ref(db, `${refPath}/${uuid}`))
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
+    // Check if user is authenticated
+    if (auth.currentUser) {
+      remove(ref(db, `${refPath}/${uuid}`))
+        .then(() => {
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    } else {
+      reject(new Error("User not authenticated")); 
+    }
   });
 };
