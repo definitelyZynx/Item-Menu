0513b3b6157736fd178de4ea57b23984778d4984
Jhoemer Muyco <69888672+definitelyZynx@users.noreply.github.com>
2024-06-15 21:39:40 +0800

    Fixed

--- src/pages/Items/Items.tsx
+++ src/pages/Items/Items.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { FaReact } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
